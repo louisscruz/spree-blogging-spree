e056bb0f39624a65e3814435c163443e951d84d4
louisscruz <louisstephancruz@me.com>
2014-08-10 18:33:00 -0400

    Delete p around content

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,6 +1,6 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
-     :styles => {:mini => '48x48#', :normal => '200x200>', :large => '600x600>', :banner => '1400x300>'},
+     :styles => {:mini => '48x48#', :normal => '200x200>', :large => '600x600>', :banner => '1400x300#'},
      :default_style => :banner,
      :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
      :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"
