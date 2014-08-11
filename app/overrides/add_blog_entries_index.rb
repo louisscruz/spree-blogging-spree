8f6d1acd8ee40ef40181310a48e5c8259358fe20
louisscruz <louisstephancruz@me.com>
2014-08-11 13:54:04 -0400

    Add if state for current page

--- app/overrides/add_blog_entries_index.rb
+++ app/overrides/add_blog_entries_index.rb
@@ -1,18 +1,20 @@
 Deface::Override.new(:virtual_path => "spree/shared/_header",
                      :name => "blog_entries_index",
                      :insert_after => "#header") do
-      "<% if !@blog_entries.empty? %>
-        <div class='index_banner'>
-          <ul id='blog_entry_entries'>
-            <% @blog_entries.each do |blog_entry| %>
-              <li>
-                <%= render :partial => 'spree/blog_entries/index_blog_banner', :locals => {:blog_entry => blog_entry} %>
-              </li>
-            <% end %>
-          </ul>
-        </div>
-        <%= paginate @blog_entries %>
-      <% else %>
-        <p id='no_entries'><%= Spree.t(:no_blog_entries) %></p>
+      "<% if current_page?(root_path) %>
+        <% if !@blog_entries.empty? %>
+          <div class='index_banner'>
+            <ul id='blog_entry_entries'>
+              <% @blog_entries.each do |blog_entry| %>
+                <li>
+                  <%= render :partial => 'spree/blog_entries/index_blog_banner', :locals => {:blog_entry => blog_entry} %>
+                </li>
+              <% end %>
+            </ul>
+          </div>
+          <%= paginate @blog_entries %>
+        <% else %>
+          <p id='no_entries'><%= Spree.t(:no_blog_entries) %></p>
+        <% end %>
       <% end %>"
     end
