db3a154ea0bb2c4df9de64eead3fe14a2752aa48
louisscruz <louisstephancruz@me.com>
2014-08-11 13:42:41 -0400

    Wrap in index banner class

--- app/overrides/add_blog_entries_index.rb
+++ app/overrides/add_blog_entries_index.rb
@@ -1,14 +1,16 @@
-Deface::Override.new(:virtual_path => "spree/home/index",
+Deface::Override.new(:virtual_path => "spree/shared/_header",
                      :name => "blog_entries_index",
-                     :insert_after => "h2") do
+                     :insert_after => "#header") do
       "<% if !@blog_entries.empty? %>
-        <ul id='blog_entry_entries'>
-          <% @blog_entries.each do |blog_entry| %>
-            <li>
-              <%= render :partial => 'spree/blog_entries/blog_entry', :locals => {:blog_entry => blog_entry} %>
-            </li>
-          <% end %>
-        </ul>
+        <div class='index_banner'>
+          <ul id='blog_entry_entries'>
+            <% @blog_entries.each do |blog_entry| %>
+              <li>
+                <%= render :partial => 'spree/blog_entries/index_blog_banner', :locals => {:blog_entry => blog_entry} %>
+              </li>
+            <% end %>
+          </ul>
+        </div>
         <%= paginate @blog_entries %>
       <% else %>
         <p id='no_entries'><%= Spree.t(:no_blog_entries) %></p>
