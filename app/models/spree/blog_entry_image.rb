fbf2d8dd3169daa57bc681f8c76f708acd099428
louisscruz <louisstephancruz@me.com>
2014-08-13 16:43:06 -0400

    Add papercip double resolution

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,6 +1,13 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
-     :styles => {:mini => '48x48#', :normal => '200x200>', :large => '600x600>', :banner => '1400x300#'},
+     :styles => {
+       :mini => '48x48#',
+       :normal => '200x200>',
+       :large => '600x600>',
+       :banner => '2800x600#'},
+     :convert_options => {
+       :banner => '-quality 30'
+     },
      :default_style => :banner,
      :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
      :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"
