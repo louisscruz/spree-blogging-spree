7099feb6171439c4ba9ebc821095da70abfee871
louisscruz <louisstephancruz@me.com>
2014-08-13 14:20:26 -0400

    update controller

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -3,6 +3,6 @@ Spree::HomeController.class_eval do
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def index
-    @blog_entries = Spree::BlogEntry.all
+    @blog_entries = Spree::BlogEntry.order('created_at desc').limit(2)
   end
 end
