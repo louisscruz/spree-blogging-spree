043ee8a64ec2634661f63a617016e7afd421fe18
louisscruz <louisstephancruz@me.com>
2014-08-13 14:34:12 -0400

    Controller limit to 1, for time being

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -3,6 +3,6 @@ Spree::HomeController.class_eval do
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def index
-    @blog_entries = Spree::BlogEntry.order('created_at desc').limit(2)
+    @blog_entries = Spree::BlogEntry.order('created_at DESC').limit(1)
   end
 end
