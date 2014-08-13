12a50af58b69dbda04b57fe94a1757763a8942cb
louisscruz <louisstephancruz@me.com>
2014-08-13 14:11:47 -0400

    Delete init pagination from home controller

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,6 +1,5 @@
 Spree::HomeController.class_eval do
   helper 'spree/blog_entries'
-  before_filter :init_pagination, :only => [:index, :tag, :archive, :author, :category]
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def index
