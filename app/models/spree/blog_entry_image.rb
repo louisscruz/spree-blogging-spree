97d757f86b4fec640f298a99afebfc742577c7f5
louisscruz <louisstephancruz@me.com>
2014-08-18 13:58:22 -0400

    Add mini and full sizes

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -3,17 +3,23 @@ class Spree::BlogEntryImage < Spree::Image
      :styles => {
        :mini => '96x96#',
        :normal => '400x400#',
-       :banner_small => ['768x300#',       :jpg, :quality => 80],
-       :banner_medium => ['1024x300#',     :jpg, :quality => 80],
-       :banner_large => ['1400x300>',      :jpg, :quality => 80],
-       :banner_small2x => ['1536x600#',    :jpg, :quality => 60],
-       :banner_medium2x => ['2048x600#',   :jpg, :quality => 60],
-       :banner_large2x => ['2800x600>',    :jpg, :quality => 60]
+       :banner_mini =>     ['480x240#',       :jpg, :quality => 80],
+       :banner_small =>    ['568x240#',       :jpg, :quality => 80],
+       :banner_medium =>   ['768x300#',       :jpg, :quality => 80],
+       :banner_large =>    ['1024x300#',      :jpg, :quality => 80],
+       :banner_full =>     ['1400x300>',      :jpg, :quality => 80],
+       :banner_mini2x =>   ['960x480#',       :jpg, :quality => 60],
+       :banner_small2x =>  ['1536x600#',      :jpg, :quality => 60],
+       :banner_medium2x => ['2048x600#',      :jpg, :quality => 60],
+       :banner_large2x =>  ['2048x600#',      :jpg, :quality => 60],
+       :banner_full2x =>   ['2800x600>',      :jpg, :quality => 60]
      },
      :convert_options => {
+       :banner_mini2x => '-set colorspace sRGB -strip -sharpen 0x0.5',
        :banner_small2x => '-set colorspace sRGB -strip -sharpen 0x0.5',
-       :banner_half2x => '-set colorspace sRGB -strip -sharpen 0x0.5',
-       :banner2x => '-set colorspace sRGB -strip -sharpen 0x0.5'
+       :banner_medium2x => '-set colorspace sRGB -strip -sharpen 0x0.5',
+       :banner_large2x => '-set colorspace sRGB -strip -sharpen 0x0.5',
+       :banner_full2x => '-set colorspace sRGB -strip -sharpen 0x0.5'
      },
      :default_style => :banner_large,
      :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
