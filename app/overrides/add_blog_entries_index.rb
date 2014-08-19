5cb70d4042d705850bcfdbe612122190826f5dbe
louisscruz <louisstephancruz@me.com>
2014-08-18 20:33:19 -0400

    Add to .each loop

--- app/overrides/add_blog_entries_index.rb
+++ app/overrides/add_blog_entries_index.rb
@@ -7,7 +7,42 @@ Deface::Override.new(:virtual_path => "spree/shared/_header",
             <ul id='blog_entry_entries' class='bxslider'>
               <% @blog_entries.each do |blog_entry| %>
                 <li>
-                  <%= render :partial => 'spree/blog_entries/index_blog_banner', :locals => {:blog_entry => blog_entry} %>
+                  <%= render :partial => 'spree/blog_entries/index_blog_banner', :locals => {:blog_entry => blog_entry}, :style => '
+                      .banner_image {
+                        height: 300px;
+                        width: 100%;
+                        background-position: center;
+                        background-repeat: no-repeat;
+                        background-image: url('<%= blog_entry.blog_entry_image.attachment.url(:banner_full) %>');
+                        background-size: 1400px 300px;
+                      }
+                      @media only screen and (max-width: 1024px) {
+                        .banner_image {
+                          background-image: url('<%= blog_entry.blog_entry_image.attachment.url(:banner_large) %>');
+                          background-size: 1024px 300px;
+                        }
+                      }
+                      @media only screen and (max-width: 768px) {
+                        .banner_image {
+                          background-image: url('<%= blog_entry.blog_entry_image.attachment.url(:banner_medium) %>');
+                          background-size: 768px 300px;
+                        }
+                      }
+                      @media only screen and (max-width: 568px) {
+                        .banner_image {
+                          height: 240px;
+                          background-image: url('<%= blog_entry.blog_entry_image.attachment.url(:banner_small) %>');
+                          background-size: 568px 240px;
+                        }
+                      }
+                      @media only screen and (max-width: 480px) {
+                        .banner_image {
+                          height: 240px;
+                          background-image: url('<%= blog_entry.blog_entry_image.attachment.url(:banner_mini) %>');
+                          background-size: 480px 240px;
+                        }
+                      }
+                  ' %>
                 </li>
               <% end %>
             </ul>
