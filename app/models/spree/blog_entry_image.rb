cda0beddcdd4d0c54367d2014e30a12c609fd2a2
louisscruz <louisstephancruz@me.com>
2014-08-19 11:10:39 -0400

    Add retina sizes.

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -2,15 +2,14 @@ class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
      :styles => {
        :mini => '96x96#',
-       :normal => '400x400#',
        :banner_mini =>     ['480x240#',       :jpg, :quality => 80],
        :banner_small =>    ['568x240#',       :jpg, :quality => 80],
        :banner_medium =>   ['768x300#',       :jpg, :quality => 80],
        :banner_large =>    ['1024x300#',      :jpg, :quality => 80],
        :banner_full =>     ['1400x300>',      :jpg, :quality => 80],
        :banner_mini2x =>   ['960x480#',       :jpg, :quality => 60],
-       :banner_small2x =>  ['1536x600#',      :jpg, :quality => 60],
-       :banner_medium2x => ['2048x600#',      :jpg, :quality => 60],
+       :banner_small2x =>  ['1136x480#',      :jpg, :quality => 60],
+       :banner_medium2x => ['1536x600#',      :jpg, :quality => 60],
        :banner_large2x =>  ['2048x600#',      :jpg, :quality => 60],
        :banner_full2x =>   ['2800x600>',      :jpg, :quality => 60]
      },
